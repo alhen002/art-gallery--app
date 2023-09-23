862b5df1309ec95eb329c3491c93fbbcf5db9493
Aleksandra <abaibik@gmail.com>
2023-09-23 17:42:03 +0200

    add font

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,17 @@
 import GlobalStyle from "../styles";
+import { Montserrat } from "next/font/google";
+
+const montserrat = Montserrat({
+  weight: ["400", "500", "700"],
+  style: ["normal", "italic"],
+  subsets: ["latin"],
+});
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
+    <main className={montserrat.className}>
       <GlobalStyle />
       <Component {...pageProps} />
-    </>
+    </main>
   );
 }
